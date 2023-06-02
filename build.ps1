268f2117f364f27771ba87eb4272abc0b0736783
Donald Hobern <dhobern@gmail.com>
2023-06-02 11:01:04 +1000

    Acanthophila and Falcipenna

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 $versionid=get-date -UFormat "%y.%j (%d %b %Y)"
 $releasedate=get-date -UFormat "%Y-%m-%d"
-sed -i -e "s/^version: .*$/version: 1.1.$versionid\r/" -e "s/^released: .*$/released: $releasedate\r/" coldp/metadata.yaml
+$versionshort=get-date -UFormat "1.1.%y.%j"
+sed -i -e "s/^version: .*$/version: 1.1.$versionid\r/" -e "s/^issued: .*$/issued: $releasedate\r/" coldp/metadata.yaml
 
 erase Gelechiidae.zip
 cd coldp
