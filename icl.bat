4aa02dbf7f021c4a3b331502fc889aa3a00aac6e
Donald Hobern <dhobern@gmail.com>
2023-10-12 23:14:51 +1100

    Sabaha species

--- icl.bat
+++ icl.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-java -cp "/Dropbox/Netbeans Projects/COLDPUtils/target/COLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar" io.github.dhobern.coldp.InteractiveCommandLine coldp/data
+java -cp "../../Dropbox/Netbeans Projects/COLDPUtils/target/COLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar" io.github.dhobern.coldp.InteractiveCommandLine coldp/data
